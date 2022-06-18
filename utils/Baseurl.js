6385c0ff71ca4aa841c9e60cd286f66a659498fd
vinay.krishna <vnmishra79@gmail.com>
2022-06-19 02:33:11 +0530

    update url

--- utils/Baseurl.js
+++ utils/Baseurl.js
@@ -1,4 +1,4 @@
-export const apiBaseUrl = "http://localhost:3000"
-// export const apiBaseUrl = "http://103.133.214.133:4018"
+// export const apiBaseUrl = "http://localhost:3000"
+export const apiBaseUrl = "http://103.133.214.133:4018"
 
 export const ImageBaseUrl = "http://103.133.214.133:4018/"
