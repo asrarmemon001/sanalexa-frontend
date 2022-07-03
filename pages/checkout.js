bc0530d36c289e2d6b48948ab075167899d1aa07
nirbhayrana123 <nirbhydesign@gmail.com>
2022-07-03 11:50:00 -0200

    checkout

---
+++ pages/checkout.js
@@ -0,0 +1,11 @@
+import Layout from "../components/layout";
+import CheckoutPageTemplate from "../templates/check-out";
+
+
+export default function Home() {
+  return (
+    <Layout>
+      <CheckoutPageTemplate /> 
+    </Layout>
+  )
+}
